369e6e2ab0ed09c599951e5f1829b5a725f32987
unknown <besonnovskiyyy@gmail.com>
2024-09-05 22:46:19 +0300

    1.2

--- assets/js/burger.js
+++ assets/js/burger.js
@@ -1,10 +1,19 @@
 const burger = document.getElementById('burger__menu');
 const burger__window = document.getElementById('burger__menu__window');
+const burgerNavElems = document.querySelectorAll('.burger__nav__elem');
 
 burger.addEventListener('click', () => {
     burger.classList.toggle('active');
     burger__window.classList.toggle('active');
     document.body.classList.toggle('body-no-scroll');
+});
+
+burgerNavElems.forEach((elem) => {
+    elem.addEventListener('click', () => {
+        burger.classList.toggle('active');
+        burger__window.classList.toggle('active');
+        document.body.classList.toggle('body-no-scroll');
+    });
 });
 
 burger__window.addEventListener('animationend', () => {
@@ -13,5 +22,14 @@ burger__window.addEventListener('animationend', () => {
     }
     else {
         burger__window.style.display = 'flex';
+    }
+});
+
+burger__window.addEventListener('animationstart', () => {
+    if (burger__window.style.zIndex === '101') {
+        burger__window.style.display = '-100000';
+    }
+    else {
+        burger__window.style.zIndex = '101';
     }
 });
--- assets/js/expand__info.js
+++ assets/js/expand__info.js
@@ -1,9 +1,17 @@
 const lines = document.querySelectorAll('.line');
+const services = document.querySelectorAll('.service');
 
 lines.forEach((line) => {
     line.addEventListener('click', () => {
         // Находим FAQ внутри line
         const faq = line.querySelector('.FAQ');
         faq.classList.toggle('active'); 
+    });
+});
+
+services.forEach((service) => {
+    service.addEventListener('click', () => {
+        // Находим FAQ внутри line
+        service.classList.toggle('active'); 
     });
 });
